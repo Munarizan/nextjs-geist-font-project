71641eb2023914329fa54b0f3b4d9a1ae6a96238
CikguBotAi <munarizan.eni@gmail.com>
2025-07-23 14:28:38 +0800

    Add .next/static/chunks/src_app_page_tsx_a09d4919._.js 1753252116890

---
+++ .next/static/chunks/src_app_page_tsx_a09d4919._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_aa0f220f._.js",
+  "static/chunks/src_948ec1f8._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_jspdf_dist_jspdf_es_min_c277e70f.js",
+  "static/chunks/node_modules_html2canvas_dist_html2canvas_3938cdad.js",
+  "static/chunks/node_modules_20f51ee6._.js"
+],
+    source: "dynamic"
+});
